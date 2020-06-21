293639a501bde3f7209f3587029caa3c206fb9eb
KevinZhan <348699531@qq.com>
2020-06-21 22:52:34 +0800

    ww

---
+++ src/main/java/me/zhyx/base/TestPC.java
@@ -0,0 +1,138 @@
+package me.zhyx.base;
+
+/**
+ * @author: yx.zh
+ * @date: 2020-06-21 08:43
+ **/
+public class TestPC {
+    public static void main(String[] args) {
+        ProductContainer productContainer = new ProductContainer();
+        new Thread(new Producer(productContainer)).start();
+        new Thread(new Consumer(productContainer)).start();
+
+    }
+}
+/**
+ * 产品
+ */
+class Product{
+    int id;
+    String name;
+
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Product setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
+
+/**
+ * 生产者
+ */
+class Producer implements Runnable{
+    ProductContainer productContainer;
+
+    public Producer(ProductContainer productContainer) {
+        this.productContainer = productContainer;
+    }
+
+    @Override
+    public void run() {
+        /**
+         * 生产1000个产品
+         */
+        for (int i = 0; i < 1000; i++) {
+            Product product = new Product(i, "第" + i + "个产品");
+            try {
+                productContainer.push(product);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+/**
+ * 消费者
+ */
+class Consumer implements Runnable{
+    ProductContainer productContainer;
+
+    public Consumer(ProductContainer productContainer) {
+        this.productContainer = productContainer;
+    }
+    @Override
+    public void run() {
+        /**
+         * 消费1000个产品
+         */
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Product pop = productContainer.pop();
+                System.out.println("消费了第"+pop.getId()+"个产品");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+/**
+ * 产品容器
+ */
+class ProductContainer{
+    Product [] products=new Product[10];
+    private int count;
+    public synchronized void push(Product product) throws InterruptedException {
+        /**
+         * 容器满了通知生产者等待，通知消费者消费
+         */
+        if(count==products.length-1){
+            System.out.println("仓库满了,等待消费者消费");
+            wait();
+        }
+        products[count]=product;
+        System.out.println("生产了第"+product.getId()+"个产品");
+        count++;
+        notifyAll();
+    }
+
+    /**
+     * 消费者消费
+     * @return
+     */
+    public synchronized Product pop() throws InterruptedException {
+        /**
+         * 判断能否消费
+         */
+        if(count==0){
+            System.out.println("消费完毕等待生产");
+            wait();
+        }
+        /**
+         * 消费
+         */
+        count--;
+        Product product=products[count];
+            notifyAll();
+        return product;
+
+    }
+}
---
+++ src/main/java/me/zhyx/base/TestPC1.java
@@ -0,0 +1,99 @@
+package me.zhyx.base;
+
+/**
+ * @author: yx.zh
+ * @date: 2020-06-21 09:31
+ * 线程信号灯通讯
+ **/
+public class TestPC1 {
+    public static void main(String[] args) {
+        TV tv = new TV();
+        new Player(tv).start();
+        new Watcher(tv).start();
+    }
+}
+
+/**
+ * 演员
+ */
+class Player extends Thread{
+    TV tv;
+
+    public Player(TV tv) {
+        this.tv = tv;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            try {
+
+                if(i%2==0){
+                    tv.paly("快乐大本营！");
+                }else {
+                    tv.paly("广告！");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
+
+/**
+ * 观众
+ */
+class Watcher extends Thread{
+    TV tv;
+
+    public Watcher(TV tv) {
+        this.tv = tv;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 100; i++) {
+                tv.watch();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/**
+ * TV
+ */
+class TV {
+    /**
+     * 演员表演，观众等待T
+     * 观众观看，演员等待F
+     */
+
+    String voice;
+    boolean flag=true;
+    public synchronized void paly(String voice) throws InterruptedException {
+        if(!flag){
+            wait();
+        }
+        System.out.println("演员表演了："+voice);
+        this.notifyAll();
+        this.voice=voice;
+        this.flag=!this.flag;
+
+    }
+    /**
+     * 观众看节目
+     */
+    public synchronized void watch() throws InterruptedException {
+        if(flag){
+            wait();
+        }
+        System.out.println("观众观看了："+this.voice);
+        this.notifyAll();
+        this.flag=!this.flag;
+
+    }
+}
