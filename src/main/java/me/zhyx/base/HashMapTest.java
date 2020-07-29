1ec336382fc4f31d9eb4b196fd4fccdc32eaa887
SkyMoreStarts <348699531@qq.com>
2020-07-29 14:25:05 +0800

    leetcode everyday commit

--- src/main/java/me/zhyx/base/HashMapTest.java
+++ src/main/java/me/zhyx/base/HashMapTest.java
@@ -1,20 +1,24 @@
 package me.zhyx.base;
 
+import java.util.HashMap;
+
 /**
  * @author: yx.zh
  * @date: 2020-07-04 13:08
  **/
 public class HashMapTest {
     public static void main(String[] args) {
-        System.out.println(comparator());
+        HashMap<String, Object> val = new HashMap<>();
     }
 
     private static boolean comparator() {
         float a= 10.0f-9.0f;
-        float b= 8.0f-7.0f;
-        if(a==b){
-            return true;
-        }
+        float b= 8.0f/2.0f;
+
+        System.out.println(b);
+//        if(a==b){
+//            return true;
+//        }
         return false;
     }
 }
